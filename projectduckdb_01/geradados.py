5d9201c42d586cd1168c01c5279ce9e948254d42
Analista Oficial <analista.oficial@outlook.com>
2025-01-16 11:32:11 -0300

    Atualizado os arquivos depois de ajustar o pre-commit 0032

--- projectduckdb_01/geradados.py
+++ projectduckdb_01/geradados.py
@@ -1,6 +1,5 @@
 import os
-import secrets
-import sys
+import random
 import time
 
 
@@ -13,7 +12,7 @@ def check_args(file_args):
             raise Exception()
     except Exception:
         print(
-            "Usage:  create_measurements.sh <positive integer number of records to create>"
+            "Usage: create_measurements.sh <positive integer number of records to create>"
         )
         print("        You can use underscore notation for large number of records.")
         print("        For example:  1_000_000_000 for one billion")
@@ -29,9 +28,8 @@ def build_weather_station_name_list():
         file_contents = file.read()
     for station in file_contents.splitlines():
         if "#" in station:
-            next
-        else:
-            station_names.append(station.split(";")[0])
+            continue
+        station_names.append(station.split(";")[0])
     return list(set(station_names))
 
 
@@ -41,7 +39,7 @@ def convert_bytes(num):
     """
     for x in ["bytes", "KiB", "MiB", "GiB"]:
         if num < 1024.0:
-            return "%3.1f %s" % (num, x)
+            return f"{num:.1f} {x}"
         num /= 1024.0
 
 
@@ -57,28 +55,16 @@ def format_elapsed_time(seconds):
     else:
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
-        if minutes == 0:
-            return f"{int(hours)} hours {int(seconds)} seconds"
-        else:
-            return f"{int(hours)} hours {int(minutes)} minutes {int(seconds)} seconds"
+        return f"{int(hours)} hours {int(minutes)} minutes {int(seconds)} seconds"
 
 
 def estimate_file_size(weather_station_names, num_rows_to_create):
     """
     Tries to estimate how large a file the test data will be
     """
-    max_string = float("-inf")
-    min_string = float("inf")
-    per_record_size = 0
-    # record_size_unit = "bytes"
-
-    for station in weather_station_names:
-        if len(station) > max_string:
-            max_string = len(station)
-        if len(station) < min_string:
-            min_string = len(station)
-        per_record_size = ((max_string + min_string * 2) + len(",-123.4")) / 2
-
+    max_string = max(len(station) for station in weather_station_names)
+    min_string = min(len(station) for station in weather_station_names)
+    per_record_size = ((max_string + min_string * 2) + len(",-123.4")) / 2
     total_file_size = num_rows_to_create * per_record_size
     human_file_size = convert_bytes(total_file_size)
 
@@ -92,24 +78,20 @@ def build_test_data(weather_station_names, num_rows_to_create):
     start_time = time.time()
     coldest_temp = -99.9
     hottest_temp = 99.9
-    station_names_10k_max = secrets.choic(weather_station_names, k=10_000)
-    batch_size = 10000  # instead of writing line by line to file, process a batch of stations and put it to disk
-    # progress_step = max(1, (num_rows_to_create // batch_size) // 100)
+    batch_size = 10000  # Process a batch of stations and write to disk
+
     print("Criando o arquivo... isso vai demorar uns 10 minutos...")
 
     try:
         with open("./data/measurements.txt", "w", encoding="utf-8") as file:
-            for s in range(0, num_rows_to_create // batch_size):
-                batch = secrets.choice()(station_names_10k_max, k=batch_size)
+            for _ in range(0, num_rows_to_create // batch_size):
+                batch = random.choices(weather_station_names, k=batch_size)
                 prepped_deviated_batch = "\n".join(
-                    [
-                        f"{station};{secrets.randbelow(coldest_temp, hottest_temp):.1f}"
-                        for station in batch
-                    ]
-                )  # :.1f should quicker than round on a large scale, because round utilizes mathematical operation
+                    f"{station};{random.uniform(coldest_temp, hottest_temp):.1f}"
+                    for station in batch
+                )
                 file.write(prepped_deviated_batch + "\n")
 
-        sys.stdout.write("\n")
     except Exception as e:
         print("Something went wrong. Printing error info and exiting...")
         print(e)
@@ -129,8 +111,7 @@ def main():
     """
     main program function
     """
-    num_rows_to_create = 1000000  # 000
-    weather_station_names = []
+    num_rows_to_create = 1_000_000
     weather_station_names = build_weather_station_name_list()
     print(estimate_file_size(weather_station_names, num_rows_to_create))
     build_test_data(weather_station_names, num_rows_to_create)
@@ -139,4 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-exit()
